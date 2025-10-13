5a28f3f8627b624852283a7fc9315c713a045ae5
a2259545313-dotcom <a2259545313@gmail.com>
2025-10-13 09:55:47 -0700

    Update marriage.js

--- marriage.js
+++ marriage.js
@@ -731,12 +731,12 @@ function drawSplash(alpha)
 	// Title
 	mContext.font = '25pt Arial Black'
 	mContext.textAlign = 'center'
-	mContext.fillText('The marriage', 400, 255)
+	mContext.fillText('The marriage Mod', 400, 255)
 
 	// Credits
 	mContext.font = '12pt Arial Black'
 	mContext.textAlign = 'right'
-	mContext.fillText('Rod Humble 2006', mScreen.width - 22, mScreen.height - 27)
+	mContext.fillText('Rod Humble 2025', mScreen.width - 22, mScreen.height - 27)
 
 }
 
